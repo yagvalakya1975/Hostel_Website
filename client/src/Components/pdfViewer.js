28b959798a39826f5084f468d00e52d1cf2e136e
sgcodes7471 <official06srinjoy@gmail.com>
2024-10-14 16:45:49 +0530

    shifted from iframe to react-pdf-viewer

--- client/src/Components/pdfViewer.js
+++ client/src/Components/pdfViewer.js
@@ -1,22 +1,24 @@
-// import { Worker, Viewer } from '@react-pdf-viewer/core';
-// import '@react-pdf-viewer/core/lib/styles/index.css'; 
-// import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-// import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css'; 
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import { zoomPlugin } from '@react-pdf-viewer/zoom';
 
 const PdfViewer = ({path}) => {
-    // const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
+    const zoomPluginInstance = zoomPlugin()
 
     return (
-        <div style={{ height: '75vh' , width:`calc(100vw - 64px)`,display:"flex" ,justifyContent:"center"}}>
-            {/* <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.0.279/build/pdf.worker.min.js`}>
+        <div className='iframe' style={{ height: '75vh' ,display:"flex" ,justifyContent:"center"}}>
+            <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.0.279/build/pdf.worker.min.js`}>
                 <Viewer
-                    fileUrl='../Files/test.pdf'
-                    plugins={[defaultLayoutPluginInstance]}
+                    fileUrl={path}
+                    plugins={[defaultLayoutPluginInstance , zoomPluginInstance]}
                     onLoadError
                 />
-            </Worker> */}
-            <iframe className="iframe" src={path} width="100%" height="100%" title="View"
-            style={{backgroundColor:'#6D3D21'}}/>
+            </Worker>
+            {/* <iframe className="iframe" src={path} width="100%" height="100%" title="View"
+            style={{backgroundColor:'#6D3D21'}}/> */}
         </div>
     );
 };
